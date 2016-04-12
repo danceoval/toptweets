69362ae7ea98e119d0956d817b696b93535f0adf
Dan Sohval <daniel.sohval@gmail.com>
2016-04-12 16:45:59 -0400

    Added Mongoose schema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,60 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
   firstName : String,
   lastName: String,
-  email: String,
-  username : String,
-  password: String
+  email: {
+    type: String,
+    index: true,
+    match: /.+\@.+\..+/
+  },
+  username : {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  role : {
+    type : String,
+    enum : ['Admin', 'User']
+  },
+  password: {
+     type : String,
+     validate : [
+        function(password) {
+          return password.length >= 5;
+        },
+        'Passwords must be at least 6 characters'
+     ]
+  },
+  created : {
+    type: Date,
+    default: Date.now
+  }
 });
+
+UserSchema.virtual('fullName').get(function(){
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var s = fullName.split(' ');
+  this.firstName = s[0] || '';
+  this.lastName = s[1] || '';
+});
+
+UserSchema.statics.findOneByUsername = function(username, cb) {
+  this.findOne({username: new RegExp(username, 'i')}, cb);
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === password;
+};
+
+UserSchema.post('save', function(next){
+  if(this.isNew) {
+    console.log("New user created");
+  } else {
+    console.log("Updated user details");
+  }
+});
+
+UserSchema.set('toJSON', {getters: true, virtuals : true});
 
 mongoose.model('User', UserSchema);
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,5 +1,15 @@
 var users = require('../../app/controllers/user.server.controllers');
 
 module.exports = function(app) {
-  app.route('/users').post(users.create);
+  app.route('/users')
+    .post(users.create)
+    .get(users.list);
+
+  app.route('/users/:userID')  
+    .get(users.read)
+    .put(users.update)
+    .delete(users.delete);
+
+
+  app.param('userId', users.userByID);  
 }
