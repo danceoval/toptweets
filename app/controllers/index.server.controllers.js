ba0e7538e646b990371cc7cf99c3c5866bd0b32f
Dan Sohval <daniel.sohval@gmail.com>
2016-04-25 16:21:57 -0400

    added bower and angular

--- app/controllers/index.server.controllers.js
+++ app/controllers/index.server.controllers.js
@@ -1,14 +1,6 @@
 exports.render = function(req, res) {
-  
-  //session mgmt
-  if(req.session.lastVisit) {
-    console.log("last visit", req.session.lastVisit);
-  }
-
-  req.session.lastVisit = new Date();
-
   res.render('index', {
     title : "Top Tweets NYC",
-    userFullName : req.user ? req.user.fullName : ''
+    user : JSON.stringify(req.user)
   });
 };
---
+++ public/application.js
@@ -0,0 +1,13 @@
+var app = angular.module(app, ['ngRoute', 'users']);
+
+app.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('!');
+}])
+
+if(window.location.hash === '#_=_'){
+  window.location.hash = '#!';
+}
+
+angular.element(document).ready(function(){
+  angular.bootstrap(document, [app]);
+});
